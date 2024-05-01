76b2b49f43429097d70f6874c467145ddfb4c264
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-05-01 23:33:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    var inputField = document.getElementById('fname'); // Get the input element by ID
+    inputField.addEventListener('blur', function() {   // Add event listener for the 'blur' event
+        inputField.value = inputField.value.toUpperCase(); // Convert the text to uppercase
+    });
+});
